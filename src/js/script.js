2c42e8668d185ec363985e2c52655c03f1006850
masterLord <vavan7520@gmail.com>
2022-01-29 19:39:55 +0300

    прошол урок по условным операторам закрепил прочитав статью и практи код простейший закоментирован возможно не понадобится но пусть будет

--- src/js/script.js
+++ src/js/script.js
@@ -24,20 +24,143 @@
 'use strict';
 
 
-let names = prompt('Как ваше имя?','');
+// let names = prompt('Как ваше имя?','');
 
 
-const personalMovieDB = {
-    name: names,
-    info: {},
-};
+// const personalMovieDB = {
+//     name: names,
+//     info: {},
+// };
 
-let a = prompt('Сколько вам лет?','');
-let b = prompt('Восколько Вы пошли в школу?','');
-let c = prompt('Восколько вы закончики школу?','');
-// let d = prompt('На сколько оцените его?','');
+// let a = prompt('Сколько вам лет?','');
+// let b = prompt('Восколько Вы пошли в школу?',''); 
+// let c = prompt('Восколько вы закончики школу?','');
+// // let d = prompt('На сколько оцените его?','');
 
-personalMovieDB.info['Моё имя ' + names] = "мой возраст " + a;
-personalMovieDB.info['Пошёл в первый класс ' + b] = 'Закончил школу ' + c;
+// personalMovieDB.info['Моё имя ' + names] = "мой возраст " + a;
+// personalMovieDB.info['Пошёл в первый класс ' + b] = 'Закончил школу ' + c;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// if (4 == 9) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error');
+// }
+
+
+
+// var select = document.querySelector('select');
+// var para = document.querySelector('p');
+
+// select.addEventListener('change', setWeather);
+
+// function setWeather() {
+//     var choice = select.value;
+
+//     if (choice === 'sunny') {
+//         para.textContent = 'Сегодня хорошо и солнечно. Носите шорты! Идите на пляж, или в парк, и купите мороженое.';
+//     } else if (choice === 'rainy') {
+//         para.textContent = 'Дождь падает за окном; возьмите плащ и зонт, и не находитесь слишком долго на улице.';
+//     } else if (choice === 'snowing') {
+//         para.textContent = 'Снег падает - морозно! Лучше всего посидеть с чашкой горячего шоколада или слепить снеговика.';
+//     } else if (choice === 'overcast') {
+//         para.textContent = 'Дождя нет, но небо серое и мрачное; он все может измениться в любую минуту, поэтому на всякий случай возьмите дождевик.';
+//     } else {
+//         para.textContent = '';
+//     }
+// }
+
+
+// var cheese = 'Cheddar';
+
+// if (cheese) {
+//     console.log('Ура! Есть сыр для приготовления бутерброда.');
+// } else {
+//     console.log('Сегодня нет сыра для бутерброда.');
+// }
+
+
+// var shoppingDone = false;
+
+// if (shoppingDone) {
+//     var childsAllowance = 10;
+// } else {
+//     var childsAllowance = 5;
+// }
+
+
+
+// var select = document.querySelector('select');
+// var html = document.querySelector('html');
+// document.body.style.padding = '10px';
+
+// function update(bgColor, textColor) {
+//   html.style.backgroundColor = bgColor;
+//   html.style.color = textColor;
+// }
+
+// select.onchange = function() {
+//   ( select.value === 'black' ) ? update('black','white') : update('white','black');
+// };
+
+// select.onchange = function() {
+//     var choice = select.value;
+//     var days = 31;
+//     if(choice === 'Февраль') {
+//     days = 28;
+//     } else if(choice === 'Апрель' || choice === 'Июнь' || choice === 'Сентябрь'|| choice === 'Ноябрь') {
+//     days = 30;
+//     }
+
+//     createCalendar(days, choice);
+//    }
+
+
+// var select = document.querySelector('select');
+// var para = document.querySelector('p');
+
+// select.addEventListener('change', setWeather);
+
+
+// function setWeather() {
+//     var choice = select.value;
+
+//     switch (choice) {
+//         case 'sunny':
+//             para.textContent = 'Сегодня хорошо и солнечно. Наденьте шорты! Идите на пляж или в парк, и купите мороженое.';
+//             break;
+//         case 'rainy':
+//             para.textContent = 'На улице дождь. Возьмите плащ и зонт, и не гуляйте слишком долго';
+//             break;
+//         case 'snowing':
+//             para.textContent = 'Идёт снег - морозно! Лучше всего посидеть с чашкой горячего шоколада или слепить снеговика.';
+//             break;
+//         case 'overcast':
+//             para.textContent = 'Дождя нет, но небо серое и мрачное; он все может измениться в любую минуту, поэтому на всякий случай возьмите дождевик.';
+//             break;
+//         default:
+//             para.textContent = '';
+//     }
+// }
+
+
+
+// switch (choice) {
+//     case "black":
+//     update('black','white');
+//     break;
+//     case "white":
+//     update('white','black');
+//     break;
+//     case "purple":
+//     update('purple','white');
+//     break;
+//     case "yellow":
+//     update('yellow','purple');
+//     break;
+//     case "psychedelic":
+//     update('green','white');
+//     break;
+//     }
