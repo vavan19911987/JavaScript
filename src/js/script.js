cd06a58486ad8be8e28630415909930df86b7002
masterLord <vavan7520@gmail.com>
2022-03-07 14:48:38 +0300

    свой скримт с циклами

--- src/js/script.js
+++ src/js/script.js
@@ -1,51 +1,33 @@
-/* Задание на урок:
-
-1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
-
-2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
-отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
-возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как 
-str.length - и получить её длину)
-
-3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
-"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
-"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
-
-4) Потренироваться и переписать цикл еще двумя способами*/
-
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt("Сколько фильмов вы смотрели", '');
+
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+    
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
+};
+
+for (let i = 0; i < 1; i++) {
+    const a = prompt("Название последнего фильма", '');
+
+    if (a != null && a != '') {
+        personalMovieDB.movies12 = a;
+        console.log('Ok');
     } else {
         console.log('error');
         i--;
     }
-
 }
 
 if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
+    console.log('Мало');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
+    console.log('Хорошо');
 } else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
+    console.log('Вы киноман');
 }
 
 console.log(personalMovieDB);
