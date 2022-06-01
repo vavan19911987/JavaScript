e8056316feb9e431f9837fdb7fc192570db1e8c2
masterLord <vavan7520@gmail.com>
2022-06-01 11:01:12 +0300

    урок 17

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+
+// const result =  confirm("Are you here?");
+
+// console.log(result);
+
+
+// const answer = prompt("Вам есть 18 лет", "18");
+
+// console.log(typeof(answer));
+
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваше фамилия?', '');
+// answers[2] = prompt('Сколько ваи лет?', '');
+
+// document.write(answers);
+
+// const category = "toys";
+
+// console.log(`https//someurl.com/${category}/5`);
+
+let incr = 10;
+let decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
